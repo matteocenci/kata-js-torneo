8a231e1619d395d96791818d04804a9f0850d52a
matteocenci <matteocenci01@gmail.com>
2024-11-25 17:10:43 +0100

    Init project: creating array of partecipants

---
+++ script.js
@@ -0,0 +1,15 @@
+
+// creo l'array dei partecipanti e il loro rispettivo potere
+
+const fighters = [
+    { name:'Freezer', power: 8000},
+    { name:'Vegeta', power: 8500},
+    { name: 'Junior', power: 6000},
+    { name: 'Goku', power: 9000},
+    { name: 'Gohan', power: 8900},
+    { name: 'Tensing', power: 450},
+    { name: 'Mr Satan', power: 50},
+    { name: 'Trunks', power: 1250},
+    { name: 'C-18', power: 7800}
+
+]
