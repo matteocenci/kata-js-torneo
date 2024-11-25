13e3be6609e05aafe8e4c925a052fe32c6256814
matteocenci <matteocenci01@gmail.com>
2024-11-25 17:20:12 +0100

    feat: created weapon array

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ const fighters = [
     { name: 'C-18', power: 7800}
 
 ]
+
+// creo l'array delle armi utilizzabili 
+
+const weapons = [
+    { name: "Scouter", power: 30},
+    { name: 'Katana di Yajirobei', power: 85},
+    { name: 'Fagioli magici', power: 60},
+    { name: 'Spada del dragon azzurro', power: 115},
+    { name: 'Armatura Saiyan', power: 145},
+    { name: 'Spada Z', power: 235},
+    { name: 'Nuvola oro', power: 200},
+    { name: 'Orecchini Potara', power: 250},
+    { name: 'Bastone Nyoi', power: 220},
+
+
+
+]
